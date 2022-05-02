c49618b7f0712a2780092a840d7e5a751374b4e5
noeru_desu <3203911711@qq.com>
2022-05-02 10:45:50 +0800

    发布: v1.4.1
    顺便修复了因常量设置错误而导致的显示详细版本号的问题

--- image_encryptor/constants.py
+++ image_encryptor/constants.py
@@ -2,7 +2,7 @@
 Author       : noeru_desu
 Date         : 2021-11-12 16:50:59
 LastEditors  : noeru_desu
-LastEditTime : 2022-05-01 13:09:09
+LastEditTime : 2022-05-02 10:43:51
 Description  : 常量
 """
 from platform import machine, platform
@@ -32,10 +32,10 @@ RELEASE_CANDIDATE = 1
 DEV = 2
 BETA = 3
 ALPHA = 4
-VERSION_TYPE = RELEASE_CANDIDATE
+VERSION_TYPE = RELEASE
 VERSION_NUMBER = '1.4.1'
-SUB_VERSION_NUMBER = 'dev.2'
-VERSION_BATCH = '20220501-2'
+SUB_VERSION_NUMBER = 'release'
+VERSION_BATCH = '20220502-1'
 BRANCH = 'dev/1.x'
 
 OPEN_SOURCE_URL = 'https://github.com/noeru-desu/Image-encryptor'
