ad0edc31c2c28f7e5decfefd23ced2cbcba38e24
noeru_desu <3203911711@qq.com>
2022-05-13 20:27:34 +0800

    修改: 预览图生成线程任务量限制为1
    `maxlen`为`1`时`Deque`将自动使用性能开销更小的`SingleTaskDeque`

--- image_encryptor/frame/preview_generator.py
+++ image_encryptor/frame/preview_generator.py
@@ -2,7 +2,7 @@
 Author       : noeru_desu
 Date         : 2021-11-13 21:43:57
 LastEditors  : noeru_desu
-LastEditTime : 2022-05-08 18:06:50
+LastEditTime : 2022-05-13 20:26:56
 Description  : 图像生成功能
 """
 from typing import TYPE_CHECKING
@@ -23,7 +23,7 @@ class PreviewGenerator(object):
 
     def __init__(self, frame: 'MainFrame'):
         self.frame = frame
-        self.preview_thread = SingleThreadExecutor('preview-thread')
+        self.preview_thread = SingleThreadExecutor('preview-thread', 1)
 
     def generate_preview(self):
         if not self.frame.update_password_dict():
@@ -35,25 +35,24 @@ class PreviewGenerator(object):
             source = self.frame.image_item.cache.initial_preview
             type_conversion = ImageData
         with self.preview_thread.thread.lock:
-            if self.preview_thread.thread.in_execution:
-                self.preview_thread.clear_task()
+            if not self.preview_thread.idle:
                 self.preview_thread.interrupt_task()
         match self.frame.controls.proc_mode:
             case ProcModes.encryption_mode:
                 self.preview_thread.add_task(encrypt.normal, (
                     self.frame, self.frame.previewProgressInfo.SetLabelText, self.frame.previewProgress,
                     source, False, type_conversion
-                ), cb=self._generate_preview_call_back, highest_priority=True)
+                ), cb=self._generate_preview_call_back)
             case ProcModes.decryption_mode:
                 self.preview_thread.add_task(decrypt.normal, (
                     self.frame, self.frame.previewProgressInfo.SetLabelText, self.frame.previewProgress,
                     self.frame.image_item.cache.loaded_image, False, PillowImage
-                ), cb=self._generate_preview_call_back, highest_priority=True)
+                ), cb=self._generate_preview_call_back)
             case ProcModes.anti_shielded_mode:
                 self.preview_thread.add_task(anti_shielded.normal, (
                     self.frame, self.frame.previewProgressInfo.SetLabelText, self.frame.previewProgress,
                     source, False, self.frame.controls.preview_source == ORIG_IMAGE
-                ), cb=self._generate_preview_call_back, highest_priority=True)
+                ), cb=self._generate_preview_call_back)
 
     def _generate_preview_call_back(self, result):
         data, error = result
