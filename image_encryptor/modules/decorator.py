cc6373359895664255baaa3331dd22ff4a8a096f
noeru_desu <3203911711@qq.com>
2022-05-13 20:12:18 +0800

    优化: Python使用`-O`参数时不使用异常捕获装饰器

--- image_encryptor/modules/decorator.py
+++ image_encryptor/modules/decorator.py
@@ -2,7 +2,7 @@
 Author       : noeru_desu
 Date         : 2022-05-08 15:16:41
 LastEditors  : noeru_desu
-LastEditTime : 2022-05-08 15:20:23
+LastEditTime : 2022-05-12 06:27:12
 Description  : 装饰器
 """
 from functools import wraps as functools_wraps
@@ -11,29 +11,37 @@ from traceback import format_exc
 from image_encryptor.utils.misc_utils import copy_signature
 from image_encryptor.utils.thread import TaskInterrupted
 
+if __debug__:
+    def catch_exc_and_return(func):
+        @functools_wraps(func)
+        def wrap(*args, **kwargs):
+            try:
+                return func(*args, **kwargs), None
+            except TaskInterrupted as e:
+                raise e from e
+            except Exception:
+                return None, format_exc()
+        copy_signature(wrap, func)
+        return wrap
+else:
+    def catch_exc_and_return(func):
+        warp = functools_wraps(func)(lambda *args, **kwargs: (func(*args, **kwargs), None))
+        copy_signature(warp, func)
+        return warp
 
-def catch_exc_and_return(func):
-    @functools_wraps(func)
-    def wrap(*args, **kwargs):
-        try:
-            return func(*args, **kwargs), None
-        except TaskInterrupted as e:
-            raise e from e
-        except Exception:
-            return None, format_exc()
-    copy_signature(wrap, func)
-    wrap.original = func
-    return wrap
 
-
-def catch_exc_for_frame_method(func):
-    @functools_wraps(func)
-    def wrap(self, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except TaskInterrupted as e:
-            raise e from e
-        except Exception:
-            self.dialog.error(format_exc())
-    copy_signature(wrap, func)
-    return wrap
+if __debug__:
+    def catch_exc_for_frame_method(func):
+        @functools_wraps(func)
+        def wrap(self, *args, **kwargs):
+            try:
+                return func(self, *args, **kwargs)
+            except TaskInterrupted as e:
+                raise e from e
+            except Exception:
+                self.dialog.error(format_exc())
+        copy_signature(wrap, func)
+        return wrap
+else:
+    def catch_exc_for_frame_method(func):
+        return func
