895a0770f31dc86b36bc48bbdfffc5f7c3a13f06
noeru_desu <3203911711@qq.com>
2022-08-17 16:04:09 +0800

    新增: 可拖动图像到幻影坦克模式设置面板作为表图

--- image_obfuscator/modes/mirage_tank/panel.py
+++ image_obfuscator/modes/mirage_tank/panel.py
@@ -2,12 +2,14 @@
 Author       : noeru_desu
 Date         : 2022-05-28 18:35:11
 LastEditors  : noeru_desu
-LastEditTime : 2022-08-15 08:44:02
+LastEditTime : 2022-08-17 16:00:53
 Description  : 
 """
+from os.path import isfile
+from traceback import format_exc
 from typing import TYPE_CHECKING
 
-from wx import BLACK, WHITE
+from wx import BLACK, WHITE, FileDropTarget, FileDirPickerEvent, PostEvent, EVT_FILEPICKER_CHANGED
 
 from image_obfuscator.modes.mirage_tank.auto_generated_panel import ProcSettingsPanel as BaseProcSettingsPanel
 from image_obfuscator.modes.base import BaseModeSettingsPanel
@@ -31,6 +33,7 @@ class ProcSettingsPanel(BaseModeSettingsPanel, BaseProcSettingsPanel):
         self.outsideImage.PickerCtrl.SetLabel('选择表图')
         self.outsideColorScale.Disable()
         self.insideColorScale.Disable()
+        self.SetDropTarget(DragOutsideImage(self))
 
     def settings_changed(self, event):
         self.main_frame.settings_changed(event)
@@ -47,3 +50,29 @@ class ProcSettingsPanel(BaseModeSettingsPanel, BaseProcSettingsPanel):
         else:
             self.toggleBg.SetLabelText('切换至\n黑底预览')
             self.main_controller.set_preview_panel_bg(WHITE)
+
+
+class DragOutsideImage(FileDropTarget):
+    __slots__ = ('panel',)
+
+    def __init__(self, panel: 'ProcSettingsPanel'):
+        super().__init__()
+        self.panel = panel
+
+    def OnDropFiles(self, x, y, filenames):
+        try:
+            filenames = tuple(filter(isfile, filenames))
+            if not filenames:
+                self.panel.main_frame.dialog.async_warning('只可拖放单个文件')
+                return True
+            elif len(filenames) > 1:
+                self.panel.main_frame.dialog.async_warning(f'共拖放了{len(filenames)}个文件，仅接受第一个文件({filenames[0]})')
+            file = filenames[0]
+            self.panel.mode_controller.outside_image_path = file
+            self.panel.main_frame.sync_setting(self.panel.outsideImage)
+        except RuntimeError:
+            return False
+        except Exception:
+            self.panel.main_frame.dialog.error(format_exc())
+        else:
+            return True
