9906e5963a2df5a43ddeb9376a36f8b3b3279177
noeru_desu <3203911711@qq.com>
2022-08-17 16:51:29 +0800

    修复: 预览图生成错误时标题未还原

--- image_obfuscator/frame/preview_generator.py
+++ image_obfuscator/frame/preview_generator.py
@@ -2,7 +2,7 @@
 Author       : noeru_desu
 Date         : 2021-11-13 21:43:57
 LastEditors  : noeru_desu
-LastEditTime : 2022-08-17 16:47:48
+LastEditTime : 2022-08-17 16:50:51
 Description  : 图像生成功能
 """
 from typing import TYPE_CHECKING
@@ -43,11 +43,11 @@ class PreviewGenerator(object):
             ), cb=self._generate_preview_call_back, cb_args=(image_item.normal_cache_hash,))
 
     def _generate_preview_call_back(self, result, cache_hash):
+        self.frame.controller.preview_panel_title = '处理结果-预览图'
         if __debug__:
             result, error = result
             if error is not None:
                 self.frame.dialog.async_error(error, '生成加密图像时出现意外错误')
                 return
-        self.frame.controller.preview_panel_title = '处理结果-预览图'
         if result is not None:
             self.frame.controller.display_and_cache_processed_preview(result, cache_hash=cache_hash)
