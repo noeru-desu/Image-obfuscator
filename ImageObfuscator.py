032738cd130d8c9b3662e54a6e72827506415a46
noeru_desu <3203911711@qq.com>
2023-03-25 19:42:17 +0800

    修改: 暂时不对multiprocessing进行引用

--- ImageObfuscator.py
+++ ImageObfuscator.py
@@ -3,9 +3,9 @@
 Author       : noeru_desu
 Date         : 2021-08-28 18:34:08
 LastEditors  : noeru_desu
-LastEditTime : 2022-08-11 13:28:33
+LastEditTime : 2023-03-25 19:34:25
 """
-from multiprocessing import freeze_support
+# from multiprocessing import freeze_support
 from warnings import filterwarnings
 
 from image_obfuscator.frame.events import MainFrame
@@ -14,5 +14,5 @@ filterwarnings('error')
 
 
 if __name__ == '__main__':
-    freeze_support()
+    # freeze_support()
     MainFrame.run()
